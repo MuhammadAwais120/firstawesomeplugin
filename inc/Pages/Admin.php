5761209dd472465b424d577a2b3860be3f145e4d
Muhammad Awais <awaismustafa3001@gmail.com>
2023-08-09 19:16:14 +0500

    More Modification in Admin Page and creation on Custom Fields and other.

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -1,40 +1,65 @@
 <?php
 /**
-    * @package firstawesome-plugin
+* @package firstawesome-plugin
 */
+
 namespace Inc\Pages;
-use \Inc\Base\BaseController;
-use \Inc\Api\SettingsApi;
+
+use Inc\Api\SettingsApi;
+use Inc\Base\BaseController;
+use Inc\Api\Callbacks\AdminCallbacks;
+
+
 class Admin extends BaseController
 {
     public $settings;
+
+	public $callbacks;
 
     public $pages = array();
 
     public $subpages = array();
 
-    public function __construct(){
-        $this->settings = new SettingsApi();
-        $this->pages = array(
+    public function register() {
+		$this->settings = new SettingsApi();
+
+		$this->callbacks = new AdminCallbacks();
+       
+		$this->setPages();
+
+		$this->setSubpages();
+
+		$this->setSettings();
+
+		$this->setSections();
+
+		$this->setFields();
+
+        $this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
+
+    }
+	public function setPages(){
+		$this->pages = array(
             [
                 'page_title' => 'Awesome Plugin',
                 'menu_title' => 'Awesome',
                 'capability' => 'manage_options',
                 'menu_slug' => 'awesome_plugin',
-                'callback' => function(){ echo '<h1>Awesome Plugin</h1>' ; },
+                'callback' => array( $this->callbacks, 'adminDashboard' ), 
                 'icon_url' => 'dashicons-store',
                 'position' => 100
             ]
         );
-
-        $this->subpages = array(
+	}
+	public function setSubpages(){
+		$this->subpages = array(
 			array(
 				'parent_slug' => 'awesome_plugin', 
 				'page_title' => 'Custom Post Types', 
 				'menu_title' => 'CPT', 
 				'capability' => 'manage_options', 
 				'menu_slug' => 'awesome_cpt', 
-				'callback' => function() { echo '<h1>CPT Manager</h1>'; }
+				'callback' => array( $this->callbacks, 'adminCpt' )
 			),
 			array(
 				'parent_slug' => 'awesome_plugin', 
@@ -42,7 +67,7 @@ class Admin extends BaseController
 				'menu_title' => 'Taxonomies', 
 				'capability' => 'manage_options', 
 				'menu_slug' => 'awesome_taxonomies', 
-				'callback' => function() { echo '<h1>Taxonomies Manager</h1>'; }
+				'callback' => array( $this->callbacks, 'adminTaxonomy' )
 			),
 			array(
 				'parent_slug' => 'awesome_plugin', 
@@ -50,20 +75,71 @@ class Admin extends BaseController
 				'menu_title' => 'Widgets', 
 				'capability' => 'manage_options', 
 				'menu_slug' => 'awesome_widgets', 
-				'callback' => function() { echo '<h1>Widgets Manager</h1>'; }
+				'callback' => array( $this->callbacks, 'adminWidget' )
+			)
+		);
+	}
+
+	public function setSettings()
+	{
+		$args = array(
+			array(
+				'option_group' => 'awesome_options_group',
+				'option_name' => 'text_example',
+				'callback' => array( $this->callbacks, 'awesomeOptionsGroup' )
+			),
+			array(
+				'option_group' => 'awesome_options_group',
+				'option_name' => 'first_name'
 			)
 		);
 
-    }
-    public function register() {
-       
-        $this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
-    }
-    
-    // public function admin_plugin_menu(){
-    //     add_menu_page( 'Awesome Plugin', 'Awesome', 'manage_options','awesome_plugin', array( $this, 'admin_index'), '', 100 );
-    // }
-    // public function admin_index(){
-    //     require_once $this->plugin_path . 'templates/admin.php';  
-    // }
+		$this->settings->setSettings( $args );
+	}
+
+	public function setSections()
+	{
+		$args = array(
+			array(
+				'id' => 'awesome_admin_index',
+				'title' => 'Settings',
+				'callback' => array( $this->callbacks, 'awesomeAdminSection' ),
+				'page' => 'awesome_plugin'
+			)
+		);
+
+		$this->settings->setSections( $args );
+	}
+
+	public function setFields()
+	{
+		$args = array(
+			array(
+				'id' => 'text_example',
+				'title' => 'Text Example',
+				'callback' => array( $this->callbacks, 'awesomeTextExample' ),
+				'page' => 'awesome_plugin',
+				'section' => 'awesome_admin_index',
+				'args' => array(
+					'label_for' => 'text_example',
+					'class' => 'example-class'
+				)
+			),
+			array(
+				'id' => 'first_name',
+				'title' => 'First Name',
+				'callback' => array( $this->callbacks, 'awesomeFirstName' ),
+				'page' => 'awesome_plugin',
+				'section' => 'awesome_admin_index',
+				'args' => array(
+					'label_for' => 'first_name',
+					'class' => 'example-class'
+				)
+			)
+		);
+
+		$this->settings->setFields( $args );
+	}
+	
+
 }
--- templates/admin.php
+++ templates/admin.php
@@ -1,1 +1,12 @@
-<h1>Hi I am Admin :)</h1>
+<div class="wrap">
+    <h1>Hi I am Admin :)</h1>
+	<?php settings_errors(); ?>
+
+	<form method="post" action="options.php">
+		<?php 
+			settings_fields( 'awesome_options_group' );
+			do_settings_sections( 'awesome_plugin' );
+			submit_button();
+		?>
+	</form>
+</div>
